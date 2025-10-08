6ccc595c8d2964f0d4af48532fb449d1e95c98c7
Caio Carvalho <caaiosin@gmail.com>
2025-10-08 14:59:20 -0300

    [eature] first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Bem-vindo à minha landing page!</h1>
+      <p>Em breve mais conteúdo aqui 🚀</p>
+    </div>
+  );
+}
+
+export default App;
